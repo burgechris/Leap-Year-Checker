892624bb37fceb27672cb22cf284b10b90311456
Christopher Burge and Christine Frank <christine@>
2019-08-20 16:32:43 -0700

    add front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,20 @@ $(document).ready(function() {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
 
-//
+//Business Logic
 
 var leapYear = function(year) {
   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
