25f4b76af43e5c115b1a658fadfc217ee6627eda
Christopher Burge and Christine Frank <christopher@>
2019-08-20 16:15:05 -0700

    add html, js, css. Meet first two BDD spec requirements.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// User Interface Logic
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
+
+//
+
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
